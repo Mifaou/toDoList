d35d0c4182053534b982cf77599943b3e8279024
mifaou <oumaima.ouhaik@hotmail.com>
2020-10-28 20:26:49 +0100

    ToDoList

---
+++ js/toDo.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+	el: "#toDo",
+	data: {
+		tasks: [],
+		taskText: "",
+	},
+	methods: {
+		addTask: function() {
+			this.tasks.push({
+				words: this.taskText,
+				done: false
+			});
+			this.taskText = "";
+		},
+		deleteTask: function(index) {
+			this.tasks.splice(index, 1);
+		},
+		deleteAll: function() {
+			this.tasks = [];
+		},
+	},
+});
